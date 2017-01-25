cf7ab7c22d4f5fda0c3cfbccd7c7891d6ec384a5
Sasa M <smikodanic@gmail.com>
2017-01-25 13:08:56 +0100

    small fix

--- server/app/routes/_routes.js
+++ server/app/routes/_routes.js
@@ -3,9 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 
-
-/********* UNIapi (WWW) ENDPOINTS *********/
 router.get('/', require('./index.js').home);
 router.get('/page', require('./index.js').page);
+
 
 module.exports = router;
